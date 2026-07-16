712f4668e9215d6cae17b3045155f689df83827a
Arpana Kumal <kumalarpana@gmail.com>
2026-07-16 18:37:04 +0545

    Working on Themess

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Home} from "./pages/Home";
+import 
+{Notfound} from "./pages/Notfound";
+
+
+
+
+function App(){
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="*" element={<Notfound/>}/>
+    
+    </Routes>
+
+
+    
+    </BrowserRouter>
+    </>
+
+  );
+}
+
+export default App;
---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+
+
+
+
+
+
+export function Home() {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+      {/**theme toggle */}
+<ThemeToggle/>
+
+
+
+
+
+
+
+
+      {/**backgroung */}
+
+
+      {/**navbar */}
+
+
+      {/**main content */}
+
+
+      {/**footer */}
+
+
+
+
+    </div>
+  );
+}
