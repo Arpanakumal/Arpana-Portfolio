d45f2be9fd1b1baf9a1259bda9c5edd09246d483
Arpana Kumal <kumalarpana@gmail.com>
2026-07-24 16:04:41 +0545

    Backend deploy

--- portfolio/backend/server.js
+++ portfolio/backend/server.js
@@ -4,6 +4,7 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -52,6 +53,6 @@ ${message}
 });
 
 
-app.listen(5000, () => {
-    console.log("Backend running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Backend running on port ${PORT}`);
 });
